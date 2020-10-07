edb96a2a9e4e735a774bb1acb5895a68edf5e053
ian <chengkwanyu@yahoo.com.hk>
2020-10-07 17:46:14 +0800

    Updates

---
+++ precache-manifest.e34b6451c9e7de79f351a83047d97039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d0f7c80ada886ba3956a1cbc7512b4",
+    "url": "/employeeDirectory/index.html"
+  },
+  {
+    "revision": "ff45e84feb354ba1ae1a",
+    "url": "/employeeDirectory/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "facdb930b202db883057",
+    "url": "/employeeDirectory/static/css/main.1967e5f8.chunk.css"
+  },
+  {
+    "revision": "ff45e84feb354ba1ae1a",
+    "url": "/employeeDirectory/static/js/2.702bbedf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employeeDirectory/static/js/2.702bbedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "facdb930b202db883057",
+    "url": "/employeeDirectory/static/js/main.d8ee4be1.chunk.js"
+  },
+  {
+    "revision": "896d028b0ca4ecbed8eb",
+    "url": "/employeeDirectory/static/js/runtime-main.6e31a4cb.js"
+  }
+]);
